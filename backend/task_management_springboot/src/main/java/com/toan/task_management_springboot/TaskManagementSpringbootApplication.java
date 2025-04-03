d388502ffdf9a67860fae07d3c590d70cc932a19
ngductoann <ngductoan0712@gmail.com>
2025-04-03 10:38:04 +0700

    first commit

---
+++ backend/task_management_springboot/src/main/java/com/toan/task_management_springboot/TaskManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.toan.task_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/task_management_springboot/src/test/java/com/toan/task_management_springboot/TaskManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toan.task_management_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
